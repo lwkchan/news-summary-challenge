4fe3d6bb1e5739f37f0bcea6e1036649a61b6015
Laura Chan <laurachan205@gmail.com>
2018-03-10 23:13:37 +0000

    Add test for JSON response in getNewsTest.js

--- test/getNewsTest.js
+++ test/getNewsTest.js
@@ -1,7 +1,19 @@
 var expect = chai.expect;
 
 describe('getNews', function(){
+
+  const link = './test/newsstub.json';
+
   it('returns a promise', function(){
-    expect(getNews('newsStub.json')).to.be.an.instanceof(Promise);
+    expect(getNews(link)).to.be.an.instanceof(Promise);
+  });
+
+  it('returns a response which can be parsed into JSON', function(){
+    return (getNews(link)).then(function(result){
+      var obj = JSON.parse(result);
+      expect(obj).to.have.property('response')
+    }).catch(function(error){
+      console.log(Error + error);
+    });
   });
 });
