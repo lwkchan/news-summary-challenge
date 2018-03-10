8a68b1dd2d57739a5856aeecddfee3da5b72f011
Laura Chan <laurachan205@gmail.com>
2018-03-10 13:43:59 +0000

    Add feature test for displaying today's headlines

--- test/newsSummaryFeature.js
+++ test/newsSummaryFeature.js
@@ -1,3 +1,5 @@
+var chai = require('chai');
+var expect = chai.expect;
 var server = require("http-server/bin/http-server");
 var Browser = require('zombie');
 Browser.localhost('newssummary.com', 8080);
@@ -12,6 +14,13 @@ describe('Browser working', function() {
 
   it('should be successful', function() {
     browser.assert.success();
+  });
+
+  it('should display Today\'s headlines', function() {
+    expect(browser.html()).to.include("Private school teacher banned for using racist language");
+    expect(browser.html()).to.include("Complaints system could be extended after MP bullying claims");
+    expect(browser.html()).to.include("Even in the era of super-clubs, football’s pleasure principle still holds");
+    expect(browser.html()).to.include("Pep Guardiola fined £20,000 by FA for wearing yellow ribbon in FA Cup");
   });
 
 });
