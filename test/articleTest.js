bbbf8645275b3eaa45a02d91b9959c6b04ad6a08
Laura Chan <laurachan205@gmail.com>
2018-03-10 16:49:55 +0000

    Add headline attribute test to articleTest.js

--- test/articleTest.js
+++ test/articleTest.js
@@ -1,8 +1,13 @@
 var expect = chai.expect
 
 describe('Article', function(){
-  it('has a headline attribute', function(){
-    var article = new Article;
+  it('is instantiated with a headline attribute', function(){
+    var article = new Article('headline');
     expect(article).to.have.property('headline');
-  })
+  });
+
+  it('headline attribute returns a string', function(){
+    var article = new Article('Russian spy attacked');
+    expect(article.headline).to.equal('Russian spy attacked');
+  });
 });
