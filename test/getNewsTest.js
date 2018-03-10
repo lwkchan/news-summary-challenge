c79858c5b462cde551d7efbdc02232ab6bcb9008
Laura Chan <laurachan205@gmail.com>
2018-03-10 17:22:24 +0000

    Modify getNewsTest.js

--- test/getNewsTest.js
+++ test/getNewsTest.js
@@ -2,7 +2,6 @@ var expect = chai.expect;
 
 describe('getNews', function(){
   it('returns a promise', function(){
-    var getNews = new getNews(newsStub.json);
-    expect(getNews).to.be.an.instanceof(Promise);
+    expect(getNews('newsStub.json')).to.be.an.instanceof(Promise);
   });
 });
