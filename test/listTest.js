c064044188f04c299be320738c27a561f0a1c6d5
Laura Chan <laurachan205@gmail.com>
2018-03-10 14:08:42 +0000

    Add listTest.js

---
+++ test/listTest.js
@@ -0,0 +1,9 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('List', function(){
+  it('should contain an array in its "articles" attribute', function(){
+    var list = new List();
+    expect(list.articles).to.be.an.instanceof(Array);
+  });
+})
