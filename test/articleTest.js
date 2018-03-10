7fdd5393c415d882bd545384c0631b264bd7a252
Laura Chan <laurachan205@gmail.com>
2018-03-10 16:30:10 +0000

    Modify articleTest.js

--- test/articleTest.js
+++ test/articleTest.js
@@ -3,6 +3,6 @@ var expect = chai.expect
 describe('Article', function(){
   it('has a headline attribute', function(){
     var article = new Article;
-    expect(article).to.respondTo('headline'));
+    expect(article).to.have.property('headline');
   })
 });
