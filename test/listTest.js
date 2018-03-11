ef943bcb361595cfeb97bc02e9e3fdef36c1b842
Laura Chan <laurachan205@gmail.com>
2018-03-11 18:48:57 +0000

    Correct listTest to add add()

--- test/listTest.js
+++ test/listTest.js
@@ -1,8 +1,21 @@
 var expect = chai.expect;
 
 describe('List', function(){
-  it('should be instantiated with an array, which will be passed it its articles attribute', function(){
-    var list = new List([]);
-    expect(list.articles).to.equal([]);
+
+  var list;
+
+  beforeEach(function(){
+    list = new List();
   });
+
+
+  it('is instantiated with an articles attribute, which is an array', function(){
+    expect(list.articles).to.be.an.instanceof(Array);
+  });
+
+  it('add() adds the specified object to the articles array', function(){
+    var article = sinon.spy();
+    list.add(article);
+    expect(list.articles.count).to.include(article);
+  })
 })
