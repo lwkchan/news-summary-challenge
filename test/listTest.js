4f7063a1fae2191dac262b8eb5b9c50a51c01385
Laura Chan <laurachan205@gmail.com>
2018-03-11 18:32:59 +0000

    Modify listTest so list takes an array as an argument at instantiation

--- test/listTest.js
+++ test/listTest.js
@@ -1,8 +1,8 @@
 var expect = chai.expect;
 
 describe('List', function(){
-  it('should contain an array in its "articles" attribute', function(){
-    var list = new List();
-    expect(list.articles).to.be.an.instanceof(Array);
+  it('should be instantiated with an array, which will be passed it its articles attribute', function(){
+    var list = new List([]);
+    expect(list.articles).to.equal([]);
   });
 })
