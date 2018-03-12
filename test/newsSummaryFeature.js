c8ea4ea66ba25211cd8bc0dc051794b276c45ee9
Laura Chan <laurachan205@gmail.com>
2018-03-12 19:23:22 +0000

    Modify index.html to add headlines. Async test in newsSummeryFeature.js test still has problem

--- test/newsSummaryFeature.js
+++ test/newsSummaryFeature.js
@@ -1,26 +1,35 @@
 var chai = require('chai');
 var expect = chai.expect;
-var server = require("http-server/bin/http-server");
+// var server = require("http-server/bin/http-server");
 var Browser = require('zombie');
+var assert = require('assert');
 Browser.localhost('newssummary.com', 8080);
 
 describe('Browser working', function() {
 
   var browser = new Browser();
 
-  before(function(done) {
-    browser.visit('/', done);
-  });
-
   it('should be successful', function() {
-    browser.assert.success();
+    browser.visit('/', function(){
+      browser.assert.success();
+    });
   });
 
-  it('should display Today\'s headlines', function() {
-    expect(browser.html()).to.include("Private school teacher banned for using racist language");
-    expect(browser.html()).to.include("Complaints system could be extended after MP bullying claims");
-    expect(browser.html()).to.include("Even in the era of super-clubs, football’s pleasure principle still holds");
-    expect(browser.html()).to.include("Pep Guardiola fined £20,000 by FA for wearing yellow ribbon in FA Cup");
-  });
+  it('should display Today\'s headlines', function(done) {
 
+    // Test still broken because of async - options either to
+    // 1) use browser.wait, passing in a function which returns
+    // a specific DOM element (https://stackoverflow.com/questions/19101258/why-does-the-zombie-js-browser-return-empty-html-when-using-bootstrap)
+    // Have tried this, but the test always seems to time out before the article is loaded.
+    // or
+    // 2) wait for browser event 'loaded' to fire, however, according to the docs,
+    // this only waits for 'some' scripts to be fully loaded
+
+    browser.visit('/');
+    browser.on("loaded", function(){
+      expect(browser.text('#article-0')).to.equal("Private school teacher banned for using racist language")
+      done();
+    })
+
+  });
 });
