85655964a635c8d5846b6e59bf2e9e5e0552a109
Laura Chan <laurachan205@gmail.com>
2018-03-20 19:12:28 +0000

    Modify news_summaries_spec.rb

--- spec/news_summaries_spec.rb
+++ spec/news_summaries_spec.rb
@@ -4,16 +4,17 @@ feature 'News Summaries' do
   it 'News summaries are on home root' do
     visit('/')
     expect(page).to have_content('News Summaries')
+    page
   end
 
   it 'Article links are displayed' do
     visit('/')
-    find_link('https://www.theguardian.com/education/2018/mar/09/private-school-teacher-banned-oundle-school-racist-language').visible?
+    find_link('Private school teacher banned for using racist language').visible?
   end
 
   it 'Clicking on the link directs user to the website' do
     visit('/')
-    click_on('https://www.theguardian.com/education/2018/mar/09/private-school-teacher-banned-oundle-school-racist-language')
-    expect(browser.current_url).to eq('https://www.theguardian.com/education/2018/mar/09/private-school-teacher-banned-oundle-school-racist-language')
+    click_on('Private school teacher banned for using racist language')
+    expect(page.current_url).to eq('https://www.theguardian.com/education/2018/mar/09/private-school-teacher-banned-oundle-school-racist-language')
   end
 end
