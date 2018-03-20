44dd3a53e31847c7088cf8b912ce32c9bbdf6636
Laura Chan <laurachan205@gmail.com>
2018-03-20 18:55:23 +0000

    Add two feature tests to news_summaries_spec.rb

--- spec/news_summaries_spec.rb
+++ spec/news_summaries_spec.rb
@@ -1,6 +1,19 @@
+# Using js-test/newsstub.json as test case
+
 feature 'News Summaries' do
-  scenario 'Testing infrastructure works' do
+  it 'News summaries are on home root' do
     visit('/')
     expect(page).to have_content('News Summaries')
+  end
+
+  it 'Article links are displayed' do
+    visit('/')
+    expect(page).to have_content('https://www.theguardian.com/education/2018/mar/09/private-school-teacher-banned-oundle-school-racist-language')
+  end
+
+  it 'Clicking on the link directs user to the website' do
+    visit('/')
+    click_on('https://www.theguardian.com/education/2018/mar/09/private-school-teacher-banned-oundle-school-racist-language')
+    expect(browser.current_url).to eq('https://www.theguardian.com/education/2018/mar/09/private-school-teacher-banned-oundle-school-racist-language')
   end
 end
