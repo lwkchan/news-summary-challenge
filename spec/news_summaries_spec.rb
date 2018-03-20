3f1a61eb3dfb2515181b75921511ddd7eaecd7c2
Laura Chan <laurachan205@gmail.com>
2018-03-20 19:37:48 +0000

    Add feature test for images in news_summaries_spec.rb

--- spec/news_summaries_spec.rb
+++ spec/news_summaries_spec.rb
@@ -1,10 +1,11 @@
 # Using js-test/newsstub.json as test case
 
+NUMBER_OF_ARTICLES = 10;
+
 feature 'News Summaries' do
   it 'News summaries are on home root' do
     visit('/')
     expect(page).to have_content('News Summaries')
-    page
   end
 
   it 'Article links are displayed' do
@@ -16,5 +17,14 @@ feature 'News Summaries' do
     visit('/')
     click_on('Private school teacher banned for using racist language')
     expect(page.current_url).to eq('https://www.theguardian.com/education/2018/mar/09/private-school-teacher-banned-oundle-school-racist-language')
+  end
+
+  it 'has an image for each article' do
+    visit('/')
+    counter = 1
+    while counter <= NUMBER_OF_ARTICLES do
+      expect(page).to have_selector("img[id='image-#{counter}']")
+      counter += 1
+    end
   end
 end
