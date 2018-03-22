7d6ebda8dfe8619b898242b952da7438a2cf51b4
Laura Chan <laurachan205@gmail.com>
2018-03-22 20:54:21 +0000

    Pass summariseNews.js tests

---
+++ src/summariseNews.js
@@ -0,0 +1,4 @@
+function summariseNews(url){
+  var requestUrl = 'http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=' + url;
+  return fetch(requestUrl);
+}
