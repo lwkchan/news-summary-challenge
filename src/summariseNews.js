39ba1daba52ba0a7242dfff436753e7fd5c045be
Laura Chan <laurachan205@gmail.com>
2018-03-22 20:58:48 +0000

    remove summariseNews.js and getNews.js

--- src/summariseNews.js
+++
@@ -1,4 +0,0 @@
-function summariseNews(url){
-  var requestUrl = 'http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=' + url;
-  return fetch(requestUrl);
-}
