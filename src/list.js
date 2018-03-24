f9429e94c13c3f50a3d9ebce2ad3a6b4f3b219a6
Laura Chan <laurachan205@gmail.com>
2018-03-24 09:59:43 +0000

    Move get news funcction to list.js

--- src/list.js
+++ src/list.js
@@ -2,8 +2,32 @@ function List() {
   this.articles = [];
 }
 
-List.prototype.add = function(articles){
-  for (var i in articles){
-    this.articles.push(articles[i]);
-  }
+List.prototype.add = function(article){
+  this.articles.push(article);
+}
+
+List.prototype.getNews = function(url){
+  var list = this;
+  return new Promise(function(resolve,reject){
+    var req = new XMLHttpRequest();
+    req.open("GET", url);
+
+    req.onload = function(){
+      if (req.status == 200){
+        var data = JSON.parse(req.response)
+        data.response.results.forEach(function(result) {
+          list.add(new Article(result));
+        });
+        resolve(req.response);
+      } else {
+        reject(Error(req.statusText));
+      }
+    };
+
+    req.onerror = function(){
+      reject(Error("Network Error"))
+    };
+
+    req.send();
+  });
 }
