890f17801fb80325c50ffaa95751007e9fb06440
Laura Chan <laurachan205@gmail.com>
2018-03-30 19:31:11 +0100

    Add newsView and newsViewTest

---
+++ spec/js-spec/newsViewTest.js
@@ -0,0 +1,58 @@
+var expect = chai.expect
+
+describe('NewsView', function(){
+
+  var newsView
+
+  beforeEach(function(){
+    MockArticle = function(){
+      this.headline = "Headline";
+      this.url = "www.example.com";
+    }
+    var mockArticle1 = new MockArticle
+    var mockArticle2 = new MockArticle
+    MockList = function(article1, article2){
+      this.articles = [article1, article2]
+    }
+    var mockList = new MockList(mockArticle1, mockArticle2)
+    newsView = new NewsView(mockList)
+  });
+
+  it('render an html string which includes the headline details', function(){
+    var render = newsView.render();
+    console.log(render)
+    expect(render).to.contain('Headline');
+    expect(render).to.contain('www.example.com');
+  });
+
+})
+
+
+// console.log(JSON.stringify(list.articles[1]))
+// console.log(2, list.articles)
+// for (var i in list.articles){
+// var article = list.articles[i];
+//
+// var div = document.createElement("div");
+// div.setAttribute("class", "article")
+// div.setAttribute("id", "article-" + i);
+//
+// var articleHeadline = document.createElement("li");
+// var headlineA = document.createElement("a");
+// headlineA.setAttribute("href", article.url)
+// var headline = document.createTextNode(article.headline);
+// headlineA.appendChild(headline);
+// articleHeadline.appendChild(headlineA);
+// div.appendChild(articleHeadline);
+//
+// var articleImage = document.createElement("img");
+// articleImage.setAttribute("src", article.imageUrl);
+// articleImage.setAttribute("id", "image-" + i)
+// div.appendChild(articleImage);
+//
+// var articleSummary = document.createElement("p");
+// summary = document.createTextNode(article.summary);
+// articleSummary.appendChild(summary)
+// div.appendChild(articleSummary)
+//
+// document.getElementById('list').appendChild(div);
---
+++ src/views/newsView.js
@@ -0,0 +1,25 @@
+(function(exports){
+  function NewsView(list){
+    this.list = list;
+  }
+
+  NewsView.prototype.render = function(){
+    var list = this.list
+    articlesHtml = ""
+    for (var i in list.articles){
+      var article = list.articles[i];
+      console.log(article)
+      var template = [
+        `<div class="article" id="article-${i}">`,
+        `<li><a href="${article.url}">${article.headline}</a></li>`,
+        `<img src="${article.imageUrl}" id="image-${i}">`,
+        `<p>${article.summary}</p></div>"`
+      ];
+      articlesHtml += template.join("");
+    };
+    return articlesHtml
+  }
+
+  exports.NewsView = NewsView;
+
+}(this))
