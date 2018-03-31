e79324a7677bc34f225c1533d8023f83d7c19ddc
Laura Chan <laurachan205@gmail.com>
2018-03-31 21:45:57 +0100

    Remove comments from newsViewTest.js

--- spec/js-spec/newsViewTest.js
+++ spec/js-spec/newsViewTest.js
@@ -24,32 +24,3 @@ describe('NewsView', function () {
     expect(render).to.contain('www.example.com')
   })
 })
-
-// console.log(JSON.stringify(list.articles[1]))
-// console.log(2, list.articles)
-// for (var i in list.articles){
-// var article = list.articles[i];
-//
-// var div = document.createElement("div");
-// div.setAttribute("class", "article")
-// div.setAttribute("id", "article-" + i);
-//
-// var articleHeadline = document.createElement("li");
-// var headlineA = document.createElement("a");
-// headlineA.setAttribute("href", article.url)
-// var headline = document.createTextNode(article.headline);
-// headlineA.appendChild(headline);
-// articleHeadline.appendChild(headlineA);
-// div.appendChild(articleHeadline);
-//
-// var articleImage = document.createElement("img");
-// articleImage.setAttribute("src", article.imageUrl);
-// articleImage.setAttribute("id", "image-" + i)
-// div.appendChild(articleImage);
-//
-// var articleSummary = document.createElement("p");
-// summary = document.createTextNode(article.summary);
-// articleSummary.appendChild(summary)
-// div.appendChild(articleSummary)
-//
-// document.getElementById('list').appendChild(div);
