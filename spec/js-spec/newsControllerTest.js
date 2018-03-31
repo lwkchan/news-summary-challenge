1a2987bca4ce821b7210ded6c539ded7f8abf4ee
Laura Chan <laurachan205@gmail.com>
2018-03-31 21:50:36 +0100

    Fix newsControllertest

--- spec/js-spec/newsControllerTest.js
+++ spec/js-spec/newsControllerTest.js
@@ -1,17 +1,19 @@
 var expect = chai.expect
+var url = 'spec/newsstub.json'
 
 describe('NewsController', function () {
   var newsController
 
   beforeEach(function () {
     var mockList = {
-      getNews: function (url) {
+      getNews: function () {
         return Promise.resolve('News retrieved')
       },
       generateSummaryPromises: function () {
         return Promise.resolve('Summary promises Generated')
       }
     }
+    mockList.articleSummaryPromises = [Promise.resolve('Summary1 retrieved'), Promise.resolve('Summary2 retrieved')]
     var mockNewsView = { render: function () { return 'Rendered' } }
     newsController = new NewsController(mockList, mockNewsView)
   })
@@ -20,11 +22,11 @@ describe('NewsController', function () {
     expect(newsController.renderPage()).to.equal('Rendered')
   })
 
-  it('retrieves the news from specified url and generates promises for retrieving article summaries', function () {
-    var url = 'spec/js-spec/newsstub.json'
+  it('retrieves the news from specified url and retrieves article summaries', function () {
     return newsController.retrieveNews(url)
       .then(function (result) {
-        expect(result).to.equal('Summary promises Generated')
+        expect(result).to.contain('Summary1 retrieved')
+        expect(result).to.contain('Summary2 retrieved')
       })
   })
 })
